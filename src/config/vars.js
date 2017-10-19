c8f97cec2e073618ac3d5857cd435c7c8d108a2a
Scott Thorpe <sthorpe@gmail.com>
2017-10-19 12:28:09 -0600

    Changed address of database

--- src/config/vars.js
+++ src/config/vars.js
@@ -1,4 +1,4 @@
 module.exports = {
   port: 8081,
-  mongo: process.env.MONGODB_URI || 'mongodb://localhost:27017/htm-project'
+  mongo: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/htm-project'
 };
